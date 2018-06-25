edeb0098a04630c472b9f0e26340d615986b8f71
Junhong-Kim <jhk1005jhk@gmail.com>
2018-06-26 00:40:44 +0900

    Add: footer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>Footer</div>
+  <nav>
+    <div class="contents">
+      <div class="copyright">
+        <ul>
+          <li><a href="http://inma.tistory.com/" target="_blank"><img class="icon" src="../../assets/tistory.png"></a></li>
+          <li><a href="https://github.com/Junhong-Kim" target="_blank"><img class="icon" src="../../assets/github.png"></a></li>
+        </ul>
+        Copyright © Junhong Kim. All rights reserved.
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -9,5 +19,38 @@ export default {
 </script>
 
 <style scoped>
+nav {
+  border-top: 1px solid #EAEAEA;
+  width: 100%;
+  height: 90px;
+  text-align: center;
+}
+
+.contents {
+  display: inline-block;
+  line-height: 45px;
+  width: 50%;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  text-decoration: none;
+  display: inline;
+  padding: 5px;
+}
+
+.icon {
+  width: 30px;
+}
+
+.copyright {
+  font-size: 15px;
+  color: #707070;
+}
 
 </style>
