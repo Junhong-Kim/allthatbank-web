91c3b8225cf7e122500d084af4c550ccbbeff3f2
Junhong-Kim <jhk1005jhk@gmail.com>
2018-07-21 00:13:13 +0900

    Modify: footer contents

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,12 +1,19 @@
 <template>
   <nav>
     <div class="contents">
-      <div class="copyright">
-        <ul>
-          <li><a href="http://inma.tistory.com/" target="_blank"><img class="icon" src="../../assets/tistory.png"></a></li>
-          <li><a href="https://github.com/Junhong-Kim" target="_blank"><img class="icon" src="../../assets/github.png"></a></li>
-        </ul>
-        Copyright © Junhong Kim. All rights reserved.
+      <div>
+        <span class="copyright">Copyright © Junhong Kim. All rights reserved.</span>
+      </div>
+      <div class="line"></div>
+      <div class="notice">
+        <div>
+        올댓뱅크는 "금융상품통합비교공시시스템(금융감독원) 금융회사 상품정보" 오픈 API 서비스를 사용한 결과입니다.
+        올댓뱅크에서 제공되는 금융회사 상품정보를 복제 또는 저장하거나 전송할 수 없습니다.
+        </div>
+        <div>
+        올댓뱅크에서 제공되는 금융회사 상품정보는 참고사항이며 오류가 발생하거나 지연될 수 있습니다.
+        올댓뱅크에서 제공되는 금융회사 상품정보에 대한 법적인 책임을 지지 않습니다.
+        </div>
       </div>
     </div>
   </nav>
@@ -20,37 +27,25 @@ export default {
 
 <style scoped>
 nav {
-  border-top: 1px solid #EAEAEA;
-  width: 100%;
-  height: 90px;
-  text-align: center;
+  background: #EAEAEA;
+  padding: 20px 0;
 }
-
 .contents {
-  display: inline-block;
-  line-height: 45px;
-  width: 50%;
+  margin: 0 auto;
+  width: 70%;
 }
-
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-}
-
-li {
-  text-decoration: none;
-  display: inline;
-  padding: 5px;
-}
-
-.icon {
-  width: 30px;
-}
-
 .copyright {
-  font-size: 15px;
-  color: #707070;
+  color: gray;
+  font-size: 14px;
+  font-weight: bold;
 }
-
+.line {
+  background: white;
+  border: 1px solid #f7f7f7;
+  margin: 10px 0;
+}
+.notice {
+  color: #777777;
+  font-size: 8px;
+}
 </style>
