a9ffd98e996ff9f1eef1cbeb53d8b32208f7a1b7
Junhong-Kim <jhk1005jhk@gmail.com>
2018-08-13 00:39:32 +0900

    Add: maintain local login status

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+import {decodeJwt, getCookie} from './utils/common'
 import AppHeader from '@/components/layout/Header'
 import AppFooter from '@/components/layout/Footer'
 import Constant from './constant'
@@ -18,7 +19,24 @@ export default {
     AppFooter
   },
   created () {
+    const token = getCookie('x-access-token')
+    if (token != null) {
+      this.setLocalUser(token)
+    }
     this.$store.dispatch(Constant.SET_BANK_LIST)
+  },
+  methods: {
+    setLocalUser (token) {
+      const user = decodeJwt(token)['user']
+      const payload = {
+        'id': user.id,
+        'username': user.username,
+        'nickname': user.nickname,
+        'picture_url': user.picture_url
+      }
+      this.$store.commit(Constant.SET_LOGIN_STATUS, {isLogin: true})
+      this.$store.commit(Constant.SET_USER, payload)
+    }
   }
 }
 </script>
