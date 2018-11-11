03fd18e4a7f194a52120fafec8215537a3bf9bd5
Junhong-Kim <jhk1005jhk@gmail.com>
2018-11-11 23:47:38 +0900

    Add: search deposit product by product name

--- src/components/deposit/ProductName.vue
+++ src/components/deposit/ProductName.vue
@@ -1,13 +1,57 @@
 <template>
-  <div>예금상품</div>
+  <div>
+    <div class="filter-box">
+      <div class="vertical-center">
+        <div class="title">찾으시는 <span class="emphasis">예금상품</span>이 있으신가요?</div>
+        <search-box :search="search"/>
+      </div>
+    </div>
+    <contents/>
+  </div>
 </template>
 
 <script>
-export default {
+import SearchBox from '@/components/common/SearchBox'
+import Contents from '@/components/deposit/Contents'
+import Constant from '../../constant'
 
+export default {
+  components: {
+    SearchBox,
+    Contents
+  },
+  data () {
+    return {
+      product: []
+    }
+  },
+  methods: {
+    search (keyword) {
+      if (keyword === '') {
+        alert('상품 이름을 입력해주세요.')
+      } else {
+        this.$http.get('/fss/deposit_products/search', {
+          params: {
+            fin_prdt_nm: keyword
+          }
+        }).then(res => {
+          const data = res.data.data
+          let payload = {
+            productList: data
+          }
+          this.$store.commit(Constant.SET_DEPOSIT_PRODUCT_LIST, payload)
+          console.log(res)
+        })
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.vertical-center {
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+}
 </style>
