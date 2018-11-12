26a0c5702260bece9336c9b297929f395823ab8e
Junhong-Kim <jhk1005jhk@gmail.com>
2018-11-12 21:30:41 +0900

    Add: search deposit product by product option

--- src/components/deposit/ProductOption.vue
+++ src/components/deposit/ProductOption.vue
@@ -1,13 +1,48 @@
 <template>
-  <div>예금옵션</div>
+  <div>
+    <div class="filter-box">
+      <div class="vertical-center">
+        <production-option-box :search="search"></production-option-box>
+      </div>
+    </div>
+    <contents/>
+  </div>
 </template>
 
 <script>
-export default {
+import Contents from '@/components/deposit/Contents'
+import ProductionOptionBox from '@/components/deposit/ProductOptionBox'
+import Constant from '../../constant'
+// import qs from 'qs'
 
+export default {
+  components: {
+    Contents,
+    ProductionOptionBox
+  },
+  methods: {
+    search (params) {
+      this.$http.get('/fss/deposit_products/search/option', {
+        params
+        // paramsSerializer: params => qs.stringify(params, {
+        //   arrayFormat: 'repeat'
+        // })
+      }).then(res => {
+        const data = res.data.data
+        let payload = {
+          productList: data
+        }
+        this.$store.commit(Constant.SET_DEPOSIT_PRODUCT_LIST, payload)
+      })
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.vertical-center {
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+}
 </style>
